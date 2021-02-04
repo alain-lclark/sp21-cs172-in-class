3d74a63daee80ec483581fd7ce06140f4667a782
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2021-02-03 18:51:34 -0800

    feat: Flesh out main and fill out two functions
    Fill out functions countNeighboringMines() and initMinefield().

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -3,13 +3,42 @@ public class Minesweeper {
     public static void main(String[] unused) {
         StdOut.println("Welcome to Minesweeper");
 
-        boolean[][] minefield = new boolean[10][10];
+        int SIZE = 10;
+        int NUM_MINES = 10;
+
+        boolean[][] minefield = new boolean[SIZE][SIZE];
+
+        initMinefield(minefield, NUM_MINES);
 
         drawMinefield(minefield);
+
+        while (true) {
+            handleMouseClick(minefield);
+            drawMinefield(minefield);
+            if (hasWon()) {
+                StdOut.println("You won!");
+                break;
+            }
+        }
+    }
+
+    public static void handleMouseClick(boolean[][] minefield) {
     }
 
     public static int countNeighboringMines(boolean[][] minefield, int x, int y) {
-        return 0;
+        int count = 0;
+        for (int x1 = x - 1; x1 < x + 1; ++x1) {
+            for (int y1 = y - 1; y1 < y + 1; ++y1) {
+                if (x1 >= 0 && x1 < minefield.length &&
+                    y1 >= 0 && y1 < minefield.length &&
+                    (x1 != x || y1 != y)) {
+                    if (minefield[x1][y1]) {
+                        ++count;
+                    }
+                }
+            }
+        }
+        return count;
     }
 
     public static boolean hasWon() {
@@ -19,8 +48,17 @@ public class Minesweeper {
     public static void uncover() {
     }
 
-    public static void initMinefield(boolean[][] minefield) {
-
+    public static void initMinefield(boolean[][] minefield, int numMines) {
+        for (int i = 0; i < numMines; ++i) {
+            int x = StdRandom.uniform(minefield.length);
+            int y = StdRandom.uniform(minefield.length);
+            if (minefield[x][y]) {
+                --i;
+            }
+            else {
+                minefield[x][y] = true;
+            }
+        }
     }
 
     public static void drawMinefield(boolean[][] minefield) {
