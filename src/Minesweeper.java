781db7a1cebd88eb38c974e5c58e30ce2255b0cf
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2021-02-08 21:26:15 -0800

    feat: Add an event loop and extend the draw function

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -7,6 +7,7 @@ public class Minesweeper {
         int NUM_MINES = 10;
 
         boolean[][] minefield = new boolean[SIZE][SIZE];
+        boolean[][] uncovered = new boolean[SIZE][SIZE];
 
         initMinefield(minefield, NUM_MINES);
 
@@ -23,6 +24,22 @@ public class Minesweeper {
     }
 
     public static void handleMouseClick(boolean[][] minefield) {
+        while (!StdDraw.isMousePressed()) {
+            // do nothing
+        }
+
+        int x = (int)Math.round(StdDraw.mouseX());
+        int y = (int)Math.round(StdDraw.mouseY());
+
+        while (StdDraw.isMousePressed()) {
+            // do nothing
+        }
+
+        uncover();
+
+        if (minefield[x][y]) {
+            StdOut.println("BOOM");
+        }
     }
 
     public static int countNeighboringMines(boolean[][] minefield, int x, int y) {
@@ -62,9 +79,12 @@ public class Minesweeper {
     }
 
     public static void drawMinefield(boolean[][] minefield) {
+        StdDraw.clear();
         StdDraw.setScale(-0.5, minefield.length - 0.5);
-        for (int i = 0; i < minefield.length; ++i) {
-            StdDraw.square(i, 0, 0.5);
+        for (int x = 0; x < minefield.length; ++x) {
+            for (int y = 0; y < minefield.length; ++y) {
+                StdDraw.square(x, y, 0.5);
+            }
         }
     }
 
