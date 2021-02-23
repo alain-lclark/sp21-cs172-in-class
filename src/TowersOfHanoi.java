c65d1a01e221faa07b85a1d70eb586e6a52f6b21
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2021-02-22 21:35:10 -0800

    feat: Add a skeleton implementation of the Towers of Hanoi problem

---
+++ src/TowersOfHanoi.java
@@ -0,0 +1,18 @@
+public class TowersOfHanoi {
+
+    /**
+     * Moves a number of discs from a source peg to a destination peg with the help,
+     * if necessary, of the helper peg.
+     *
+     * A peg is identified by its single letter label (e.g., 'A', 'B', 'C').
+     *
+     * @param n  the number of discs to be moved
+     * @param source  the source peg where these n discs are located
+     * @param destination  the destination peg
+     * @param helper  the helper peg
+     */
+    static public void move(int n, char source, char destination, char helper) {
+        // ...
+    }
+
+}
