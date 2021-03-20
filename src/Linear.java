969ee605ca36c2af43a571d0452c499e8198e557
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2021-03-19 20:00:23 -0700

    feat: Add two implementations of the Function interface

---
+++ src/Linear.java
@@ -0,0 +1,5 @@
+public class Linear implements Function {
+
+    public double evaluate(double x) { return x; }
+
+}
---
+++ src/Quadratic.java
@@ -0,0 +1,5 @@
+public class Quadratic implements Function {
+
+    public double evaluate(double x) { return x*x; }
+
+}
